dda4eda9c981458c114543c4d9f7f8cd3910dee0
keidy1402 <fgvkeidy@gmail.com>
2026-04-28 15:29:11 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -2,6 +2,8 @@ import streamlit as st
 from PIL import Image
 
 st.title ('Teste ECMI 2')
-st.write("Esse é o meu texto")
+st.write("Aqui estão as suas notas:")
 
 st.image("https://github.com/keidy1402/projeto1/blob/main/au%20au%20fofinho.jpg")
+use_column_width=True
+PIL.Image.open()
