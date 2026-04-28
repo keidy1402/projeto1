943d206c8b8d4ba48e9c25184b575a6544df691e
keidy1402 <fgvkeidy@gmail.com>
2026-04-28 15:26:49 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -5,5 +5,4 @@ st.write("Esse é o meu texto")
 import streamlit as st
 from PIL import Image
 
-url = https://github.com/keidy1402/projeto1/blob/main/au%20au%20fofinho.jpg
-st.image(url, caption="Imagem de URL", use_column_width=True)
+st.image("https://github.com/keidy1402/projeto1/blob/main/au%20au%20fofinho.jpg")
