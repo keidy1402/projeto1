e2bfd1c6ff6a79ed6350881956da4a8b93cb604c
keidy1402 <fgvkeidy@gmail.com>
2026-04-28 15:23:26 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -5,16 +5,9 @@ st.write("Esse é o meu texto")
 import streamlit as st
 from PIL import Image
 
-# Título do app
-st.title("Exemplo de Exibição de Imagem no Streamlit")
-
 # Carregar imagem local
 try:
-    image = Image.open("minha_imagem.jpg")  # Substitua pelo caminho da sua imagem
+    image = Image.open("https://github.com/keidy1402/projeto1/blob/main/au%20au%20fofinho.jpg")  
     st.image(image, caption="Minha Imagem Local", use_column_width=True)
 except FileNotFoundError:
     st.error("Imagem não encontrada. Verifique o caminho do arquivo.")
-
-# Exibir imagem a partir de uma URL
-url = "https://via.placeholder.com/300"
-st.image(url, caption="Imagem de URL", use_column_width=True)
