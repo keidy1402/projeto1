8bd00bc001bc5fc258b0a5c2e4d9ae6dab3310c2
keidy1402 <fgvkeidy@gmail.com>
2026-04-28 15:27:18 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,8 +1,7 @@
 import streamlit as st
+from PIL import Image
 
 st.title ('Teste ECMI 2')
 st.write("Esse é o meu texto")
-import streamlit as st
-from PIL import Image
 
 st.image("https://github.com/keidy1402/projeto1/blob/main/au%20au%20fofinho.jpg")
